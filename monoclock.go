f3ecfdc3deeb6ae8173210c815af696733200e70
pavben <github@pavelbennett.com>
2013-10-30 21:00:01 -0700

    initial source commit

---
+++ monoclock.go
@@ -0,0 +1,45 @@
+package monoclock
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+type MonoTimer struct {
+	startTime int64
+}
+
+// Create a new monotonic timer and start it
+func NewMonoTimer() *MonoTimer {
+	startTime := GetMonoTime()
+
+	return &MonoTimer {
+		startTime: startTime,
+	}
+}
+
+func (self *MonoTimer) Value() int64 {
+	currentTime := GetMonoTime()
+
+	return currentTime - self.startTime
+}
+
+// Returns the time of the system monotonic clock in milliseconds
+func GetMonoTime() int64 {
+	sec, nsec := getRawMonoTime()
+
+	// to milliseconds
+	return sec * 1000 + (nsec / (1 * 1000 * 1000))
+}
+
+const CLOCK_MONOTONIC_RAW = 4
+
+func getRawMonoTime() (int64, int64) {
+	var timeSpec syscall.Timespec
+
+	syscall.Syscall(syscall.SYS_CLOCK_GETTIME, CLOCK_MONOTONIC_RAW, uintptr(unsafe.Pointer(&timeSpec)), 0)
+
+	sec, nsec := timeSpec.Unix()
+
+	return sec, nsec
+}
